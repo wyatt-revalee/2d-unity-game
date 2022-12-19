4066f83864edab8cee105ff5f0bad72a116581be
Wyatt Revalee <wyatt.revalee@gmail.com>
2022-12-18 22:51:53 -0500

    Mana regen on fixed update now. UI should scale correclty now

--- Assets/Scripts/Game/Camera/followPlayer.cs
+++ Assets/Scripts/Game/Camera/followPlayer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class followPlayer : MonoBehaviour
 {
@@ -28,6 +30,9 @@ public class followPlayer : MonoBehaviour
 
 
         transform.position = player.transform.position + new Vector3(-1, 0, -5);
+
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+            Destroy(gameObject);
         
     }
 }
